bbb8387f211d276e42e4c5bff0d229cc4fb421f7
Vasll <valentinosoll17@gmail.com>
2022-10-26 20:10:20 +0200

    Added sensor parsing logic
    Added blockToMap()
    Added trimAll()

--- src/main/kotlin/LibreParser.kt
+++ src/main/kotlin/LibreParser.kt
@@ -1,7 +1,5 @@
 import WinUtils.Powershell.LIBRE_SCRIPT
-import model.Component
 import model.Sensor
-import java.util.*
 
 /**
  * Parses the output of the LibreHardwareMonitor library into Sensor and Component objects.
@@ -11,7 +9,10 @@ import java.util.*
  * If you want to see the raw output of libreScript.ps1 use: WinUtils.Powershell.runAndGet(LIBRE_SCRIPT)
  */
 class LibreParser {
-
+    /**
+     * Since sensors in the LibreHardwareMonitor library have all the same format we can use an enum class for indexing
+     * each block code that contains sensor data
+     */
     enum class SensorParameter(val str: String) {
         CONTROL("Control"), HARDWARE("Hardware"),
         IDENTIFIER("Identifier"), INDEX("Index"),
@@ -27,45 +28,77 @@ class LibreParser {
 
             for(block in rawOutput.split("\n\n")) {
                 if(block.contains("HardwareType")){ // If the current block is a hardware/component block
-                    //val component = getComponent(block)
-                    //println(component)
+
                 }else{  // If the current block is a sensor block
-                    val sensor = getSensor(block)
-                    println(sensor)
+                    //val sensor = getSensor(block)
+                    //println(sensor)
+                    println(getSensor(block))
                 }
             }
         }
 
-        private fun getComponent(block: String): Component? {
-            return null
-        }
 
         /**
          * Returns a Sensor from a block of Strings
          */
         private fun getSensor(block: String): Sensor {
-            val lines = block.split('\n').toMutableList()
-
-            for(i in 0 until lines.size){
-                lines[i] = lines[i].split(":", limit = 2)[1].trim()
-            }
+            val blockMap = blockToMap(block)
 
             return Sensor(
-                lines[SensorParameter.HARDWARE.ordinal],
-                lines[SensorParameter.IDENTIFIER.ordinal],
-                lines[SensorParameter.INDEX.ordinal].toInt(),
-                lines[SensorParameter.NAME.ordinal],
-                lines[SensorParameter.SENSOR_TYPE.ordinal].toSensorType(),
-                lines[SensorParameter.VALUE.ordinal].toDouble(),
+                blockMap[SensorParameter.HARDWARE.str],
+                blockMap[SensorParameter.IDENTIFIER.str],
+                blockMap[SensorParameter.INDEX.str]?.toInt(),
+                blockMap[SensorParameter.NAME.str],
+                blockMap[SensorParameter.SENSOR_TYPE.str]?.toSensorType(),
+                blockMap[SensorParameter.VALUE.str]?.replace(',','.')?.toDouble(),
+                // TODO for some reason double parsing works on my AMD pc (XX.XX) but on my Intel pc double values use a comma (XX,XX)
             )
         }
 
+        /**
+         * Gets a block of code from the LibreHardwareMonitor library and gets back a map where its key is the field name
+         * and its values are the field values
+         */
+        private fun blockToMap(block: String): Map<String, String>{
+            val map = mutableMapOf<String, String>()
+
+            var lastKey = ""
+            for(line in block.split('\n')){
+                if(line.startsWith(" ")){
+                    map[lastKey] = map[lastKey] + line.trim()
+                    continue
+                }
+
+                val keyValuePair = line.split(":", limit = 2).trimAll()
+                lastKey = keyValuePair[0]
+                map[keyValuePair[0]] = keyValuePair[1]
+            }
+
+            return map
+        }
+
+        /**
+         * Gets the Sensor.Type from a String
+         */
         private fun String.toSensorType(): Sensor.Type? {
             for(type in Sensor.Type.values())
                 if(type.str == this)
                     return type
 
             return null
+        }
+
+        /**
+         * Trims all the strings in a List
+         */
+        private fun List<String>.trimAll(): List<String>{
+            val mutableList = this.toMutableList()
+
+            for(i in 0 until mutableList.size){
+                mutableList[i] = mutableList[i].trim()
+            }
+
+            return mutableList.toList()
         }
     }
 }
