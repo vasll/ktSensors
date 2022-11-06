0c730544de2391f41938942b81c075b3a636eadb
Vasll <valentinosoll17@gmail.com>
2022-11-06 18:31:00 +0100

    Moved UpdateVisitor class in separate file

--- src/main/resources/lib/win/libreScript.ps1
+++ src/main/resources/lib/win/libreScript.ps1
@@ -1,10 +1,8 @@
-# Script for getting the raw sensor data from the LibreHardwareMonitor library
-# Link:   https://github.com/LibreHardwareMonitor/LibreHardwareMonitor
-# Author: https://github.com/vasll
-
-# Load the DLLs (Make sure that the dll folder is in the same directory as the script)
+# Load DLLs
 [Reflection.Assembly]::LoadFrom($PSScriptRoot+"\dlls\LibreHardwareMonitorLib.dll") | Out-Null
 [Reflection.Assembly]::LoadFrom($PSScriptRoot+"\dlls\HidSharp.dll") | Out-Null
+
+. $PSScriptRoot\UpdateVisitor.ps1
 
 # Insantiate the computer Object
 $computer = New-Object LibreHardwareMonitor.Hardware.Computer
@@ -17,25 +15,21 @@ $computer.IsNetworkEnabled = $false
 $computer.IsStorageEnabled = $true
 $computer.IsBatteryEnabled = $true
 
+
 # Try opening
 try{
+    $UpdateVisitor = New-Object UpdateVisitor
     $computer.Open()
+    $computer.Accept($UpdateVisitor);
 }catch{
     Write-Warning $Error[0]
     Exit 69420
 }
 
+
 # Print the sensor data
-ForEach ($hardware in $computer.Hardware){
-    
+ForEach ($hardware in $computer.Hardware){ 
     $hardware
-    try{
-        $hardware.Update()
-    }catch{
-        #TODO: Here an exception is being ignored be aware
-        #Write-Warning $Error[0]
-    }
-    
     ForEach ($subHardware in $hardware.SubHardware){
         $subHardware
         ForEach ($sensor in $subHardware.Sensors){
@@ -45,5 +39,5 @@ ForEach ($hardware in $computer.Hardware){
     ForEach ($sensor in $hardware.Sensors){
         $sensor
     }
-    $computer.Close()
 }
+$computer.Close()
